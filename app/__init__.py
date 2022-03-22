66af0bd060f67bd806ac02d197d646325e038b14
AaronCeciber <3823451@alu.murciaeduca.es>
2022-03-22 18:00:19 +0100

    Subida final tar0301

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,21 @@
 from flask import Flask, render_template, request
+from flask_migrate import Migrate
 
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-
+app.secret_key = "ClaveSecreta"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456@localhost:5432/tareaTema3Project'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from .public import public
 from .private import private
 
 def create_app():
+
     app.register_blueprint(public)
     app.register_blueprint(private)
     return  app
--- app/private/routes.py
+++ app/private/routes.py
@@ -1,9 +1,38 @@
+import base64
+
 from flask import render_template
 
 from . import private
+from .models import Cliente
 
+from .forms import FormWTF, Filtrocliente
+from werkzeug.datastructures import CombinedMultiDict
+from flask import request
+
+@private.route("/altacliente/", methods=["GET","POST"])
+def altacliente():
+    form = FormWTF(CombinedMultiDict((request.files, request.form)))
+    # form = FormWTF(request.form)
+    if form.validate_on_submit():
+        form.nombre.data
+        cliente = Cliente()
+        cliente.nombre = form.nombre.data
+        cliente.dni = form.dni.data
+        cliente.apellidos = form.apellidos.data
+
+        encoded_bytes = base64.b64encode(form.imagen.data.read())
+        encoded_string = str(encoded_bytes).replace("b'", "").replace("'", "")
+        cliente.imagen = encoded_string
+        cliente.create()
+
+    return render_template("altacliente.html", form = form)
 
 
 @private.route("/indexcliente/", methods=["GET","POST"])
 def indexcliente():
-    return render_template("indexcliente.html")
+    filtro = Filtrocliente(request.form)
+    if filtro.validate_on_submit():
+        clientes = Cliente.query.filter_by(dni=filtro.dni.data)
+    else:
+        clientes = Cliente.query.all()
+    return render_template("indexcliente.html", clientes = clientes, filtro = filtro)
