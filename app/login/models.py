caa56ab20eb5ab1a7f0671a91db2396f54f844fd
AaronCeciber <3823451@alu.murciaeduca.es>
2022-03-24 19:26:02 +0100

    añado login tar0302

---
+++ app/login/models.py
@@ -0,0 +1,45 @@
+import os
+
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from app import db
+
+
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(15), unique=True,nullable=False)
+    password = db.Column(db.String(), nullable=False)
+    nombre = db.Column(db.String(20),nullable=False)
+    apellidos = db.Column(db.String(50),nullable=False)
+
+    def __str__(self):
+        return self.nombre + self.apellidos
+
+    def create(self):
+        try:
+            db.session.add(self)
+            db.session.commit()
+        except:
+            raise
+
+    @staticmethod
+    def get_by_id(id):
+        return Usuario.query.get(id)
+
+    @staticmethod
+    def get_by_username(username):
+        return Usuario.query.filter_by(username=username).first()
+
+
+    def set_password(self, password):
+        # self.password = generate_password_hash(password, method='pbkdf2:sha512')
+        method = "pbkdf2:sha256:260000"
+        # method = "plain"
+        # method = "pbkdf2:sha512:10000000"
+        self.password = generate_password_hash(password,method=method) #Por defecto sha256
+        # self.password = password.split("$", 1)[1]
+
+
+    def check_password(self, password):
+        # passHash = 'pbkdf2:sha256:260000$' + self.password
+        return check_password_hash(self.password, password)
--- app/private/forms.py
+++ app/private/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, IntegerField
+from wtforms import StringField, SubmitField, IntegerField, PasswordField
 from flask_wtf.file import FileRequired, FileAllowed, FileField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, ValidationError
 
 class FormWTF(FlaskForm):
 
@@ -35,3 +35,50 @@ class Filtrocliente(FlaskForm):
         DataRequired(message="El campo DNI es obligatorio"),
         Length(max=10, message="El campo DNI debe ser de 10 caracteres")])
     submit = SubmitField(label="Filtrar")
+
+
+class UsuarioForm(FlaskForm):
+
+    username = StringField(label="Nombre de usuario", validators=[
+        DataRequired(message="El nombre de usuario es obligatorio"),
+        Length(max=15, message="El nombre de usuario no puede ser superior a 20 caracteres")
+    ])
+
+    password = PasswordField(label="Contraseña", validators=[
+        DataRequired(message="La contraseña es obligatoria"),
+        Length(min=8, message="La contraseña debe tener al menos 8 caracteres")
+    ])
+
+    nombre = StringField(label="Nombre", validators=[
+        DataRequired(message="El nombre es obligatorio"),
+        Length(max=20, message="El nombre no puede ser superior a 20 caracteres")
+    ])
+
+    apellidos = StringField(label="Apellidos", validators=[
+        DataRequired(message="Los apellidos son obligatorio"),
+        Length(max=50, message="Los apellidos no pueden superar los 50 caracteres")
+    ])
+
+    # submit = SubmitField(label="Dar de alta")
+
+    def validate_password(form,field):
+        if str(field.data).isdigit():
+            raise ValidationError("La contraseña no pueden ser solo dígitos")
+        if field.data != form.passwordRepeat.data:
+            raise ValidationError("No coinciden las contraseñas")
+
+    def validate_passwordRepeat(form, field):
+        if field.data != form.password.data:
+            raise ValidationError("No coinciden las contraseñas")
+
+
+class LoginForm(FlaskForm):
+    username = StringField(label="Nombre de usuario", validators=[
+        DataRequired(message="El nombre de usuario es obligatorio"),
+        Length(max=15, message="El nombre de usuario no puede ser superior a 15 caracteres")
+    ])
+
+    password = PasswordField(label="Contraseña", validators=[
+        DataRequired(message="La contraseña es obligatoria"),
+        Length(min=8, message="La contraseña no puede ser inferior a 8 caracteres")
+    ])
